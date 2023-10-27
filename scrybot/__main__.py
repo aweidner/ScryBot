612050524bdab7ca6b41760b4f1a550890ca750c
Adam Weidner <adam.weidner@logrhythm.com>
2023-10-27 17:59:35 -0400

    adds requirements.txt file

--- scrybot/__main__.py
+++ scrybot/__main__.py
@@ -1,25 +1,8 @@
 import sys
+import os
 
-import discord
-
-from scrybot.parser import parser
 from scrybot import bot
 
-
-args = parser.parse_args()
-
-# Read the token from the provided file
-try:
-    with open(args.token_file, 'r') as file:
-        token = file.read().strip()
-except FileNotFoundError:
-    print("Error: Token file not found.", file=sys.stderr)
-    sys.exit(1)
-
-# Check if the token is present
-if not token:
-    print("Error: Token not found in the provided file.", file=sys.stderr)
-    sys.exit(1)
-
 # Run the bot with the provided token
+token = os.environ.get('DISCORD_BOT_TOKEN')
 bot.run(token)
--- scrybot/parser.py
+++
@@ -1,5 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description='Run a Discord bot.')
-parser.add_argument('channel_name', help='Name of the channel to monitor')
-parser.add_argument('token_file', help='Path to a file containing the Discord bot token')
